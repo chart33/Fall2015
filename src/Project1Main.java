e824ed688739a555075d736d13cb94e46d8240f5
ubuntu <ubuntu@engineering>
2015-09-13 18:44:24 -0400

    have buffered reader working.  now need to figure out how to parse values

--- src/Project1Main.java
+++ src/Project1Main.java
@@ -5,6 +5,7 @@ public class Project1Main {
 		Project1Scheduler scheduler = new Project1Scheduler();
 		scheduler.calculateSchedule("");
 		System.out.println("Now running main method to access scheduler");
-	}
-
+		Reader reader = new Reader();
+		reader.readFile("/home/ubuntu/Documents/student_schedule.txt");
+	}		
 }
--- src/Reader.java
+++ src/Reader.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.StringWriter;
+import sun.misc.IOUtils;
 
 
 public class Reader {
@@ -8,17 +10,23 @@ public class Reader {
  * text file into memory. */
 	
 //hardcode filepath: /home/ubuntu/Documents/student_schedule.txt
+//REMEMBER TO REMOVE THE HARDCODED FP BEFORE YOU SUBMIT.
 	
 public void readFile(String filePath){
 	/*TODO implement this method to read values from file into memory via a buffered reader
 	 * This will be used by the main class*/
-	 BufferedReader br;
+	 BufferedReader br = null;
 	 try{
 		br = new BufferedReader(new FileReader(filePath));
 	 String line = br.readLine();
 	 while (line != null){
 		 line = br.readLine();
 		 //can I add code here such that each line read is added to the vector?
+		 System.out.println("********************"+line);
+		//here you need to figure out a way to go from the buffered stream to string or parse to array
+		 /*StringWriter writer = new StringWriter();
+		 IOUtils.copy(inputStream, writer, encoding);
+		 String theString = writer.toString();*/
 		 
 	 }
 	 }
@@ -36,7 +44,7 @@ public void readFile(String filePath){
 	 }
 }
 
-public vector<10>{}
+
 }
 	 
 
