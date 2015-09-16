5d06f1b0c18c2610614d6ad610ad8e6299b29ea6
ubuntu <ubuntu@engineering>
2015-09-16 01:45:01 -0400

    constraints.... @*$(@&!)

--- src/Project1Scheduler.java
+++ src/Project1Scheduler.java
@@ -16,7 +16,7 @@ public class Project1Scheduler implements scheduler {
 		int student = 600;
 	    int course = 18;
 	    int semester = 12;
-
+	    
 
 	    try {
 	      GRBEnv env = new GRBEnv();
@@ -25,13 +25,15 @@ public class Project1Scheduler implements scheduler {
 	      // Create 3-D array of model variables
 
 	      GRBVar[][][] vars = new GRBVar[student][course][semester];
+	      GRBVar[] x = new GRBVar[1];//initialized to 1 because should only hold one value?
+	      
 
 	      for (int i = 0; i < student; i++) {
 	        for (int j = 0; j < course; j++) {
-	          for (int v = 0; v < semester; v++) {
+	          for (int k = 0; k < semester; k++) {
 	            String st = "G_" + String.valueOf(i) + "_" + String.valueOf(j)
-	                             + "_" + String.valueOf(v);
-	            vars[i][j][v] = model.addVar(0.0, 1.0, 0.0, GRB.BINARY, st);
+	                             + "_" + String.valueOf(k);
+	            vars[i][j][k] = model.addVar(0.0, 1.0, 0.0, GRB.BINARY, st);
 	          }
 	        }
 	      }
@@ -40,7 +42,7 @@ public class Project1Scheduler implements scheduler {
 
 	      model.update();
 
-	      // Add constraints
+	      //****************************** Add constraints***************************************
 
 	      GRBLinExpr expr;
 
@@ -57,7 +59,44 @@ public class Project1Scheduler implements scheduler {
 	        model.addConstr(expr, GRB.EQUAL, 2.0, twoClassMax);
 	    }
 	      
-	      //constraint 2: class capacity x
+	      //constraint 2: class capacity <= x
+	      for (int i = 0; i < student; i++) {
+	    	  expr = new GRBLinExpr();
+	        for (int j = 0; j < course; j++) {
+	        	for(int k = 0; k < semester; k++){
+	          expr.addTerm(1, vars[i][j][k]);
+	          //expr.addTerm(1, x[0]);
+	        	}
+	        }
+	        String maxCapacity = "MaxCap: Student"+ String.valueOf(student)+".Course"+String.valueOf(course)+".semester"+String.valueOf(semester);
+	        model.addConstr(expr, GRB.LESS_EQUAL,10000, maxCapacity);
+	      }
+	      
+	      //CONSTRAINT 3: classes needed (parse student input!)
+	      //iterating through semesters.  =1 if student wants course, =0 if student doesnt want course
+	      for (int i = 0; i < student; i++) {
+	    	  expr = new GRBLinExpr();
+	        for (int j = 0; j < course; j++) {
+	        	for(int k = 0; k < semester; k++){
+	          expr.addTerm(1, vars[i][j][k]);
+	        	}
+	        }
+	        String needsClass = "Student"+ String.valueOf(student)+".Course"+String.valueOf(course)+".semester"+String.valueOf(semester);
+	        model.addConstr(expr, GRB.EQUAL, 2.0, needsClass);
+	    }
+	      
+	      //CONSTRAINT 4: prerequisites
+	      for (int i = 0; i < student; i++) {
+	    	  expr = new GRBLinExpr();
+	        for (int j = 0; j < course; j++) {
+	        	for(int k = 0; k < semester; k++){
+	          expr.addTerm(1, vars[i][j][k]);
+	        	}
+	        }
+	        String twoClassMax = "Student"+ String.valueOf(student)+".Course"+String.valueOf(course)+".semester"+String.valueOf(semester);
+	        model.addConstr(expr, GRB.EQUAL, 2.0, twoClassMax);
+	    }
+	      
 	      
 	      
 	      // Update model
@@ -71,9 +110,9 @@ public class Project1Scheduler implements scheduler {
 	      model.optimize();
 
 	      // Write model to file
-	      model.write("sudoku.lp");
+	      model.write("scheduler.lp");
 
-	      double[][][] x = model.get(GRB.DoubleAttr.X, vars);
+	     // double[][][] x = model.get(GRB.DoubleAttr.X, vars);
 
 	    
 
